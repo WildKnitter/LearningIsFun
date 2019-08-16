4caf0ddc130b9a7a1bf012ef785c422ac548c97e
Pamela Belknap <pbelknap@cox.net>
2019-08-16 18:48:37 -0400

    Learning is Fun project! First Draft

---
+++ public/scripts/courses.js
@@ -0,0 +1,93 @@
+"use strict";
+//This scripts contains code to dynamically create information on different courses from a JSON file.
+//Author:  Pam Belknap
+
+/*
+Key for understanding how to access the fields in the JSON file:
+objs
+objs.length
+objs[i].CourseId
+objs[i].Title
+objs[i].Category
+objs[i].Location
+objs[i].StartDate
+objs[i].EndDate
+objs[i].Meets
+objs[i].Fee
+objs[i].Students (its own array)
+
+Categories:
+categ
+categ.length
+categ[i].Category
+categ[i].Value
+*/
+
+$(function() {
+        let categ;
+
+        //Starts the communication to the server
+        $.getJSON(
+            "/api/categories",
+            //This function doesn't necessarily run instantaneously
+            function(data) {
+                categ = data;
+                //load dropdown lists here (code)
+                for (let i = 0; i < categ.length; i++) {
+                    let courseOption = $("<option>", { text: categ[i].Category, value: categ[i].Value });
+                    $("#courseChoice").append(courseOption);
+                } // end of for
+                createCourseTable();
+            } // end of CALLBACK function
+        ); // end of call to $.getJSON                 
+
+        $("#courseChoice").on("change", createCourseTable);
+        $("#btnAllCourses").on("click", createCourseTableAll);
+
+        // Function to create a table for courses.
+        function createCourseTable() {
+            let objs;
+            //Starts the communication to the server
+            $.getJSON(
+                "/api/courses/bycategory/" + $("#courseChoice").val(),
+                //This function doesn't necessarily run instantaneously
+                function(data) {
+                    objs = data;
+                    $("#courseTableHead").empty();
+                    let markupHeader = "<tr><th>" + "Course ID" + "</th><th>" + "Course Name" + "</th><th>" + "Link" + "</th></tr>";
+                    $("#courseTableHead").append(markupHeader);
+                    $("#courseTableHead").css("font-weight", "bold");
+                    $("#courseTableBody").empty();
+                    for (let i = 0; i < objs.length; i++) {
+                        let url = "details.html?courseid=" + objs[i].CourseId;
+                        let markupBody = "<tr><td>" + objs[i].CourseId + "</td><td>" + objs[i].Title + "</td><td><a href=" + url + ">View Details</a></td >< /tr>";
+                        $("#courseTableBody").append(markupBody);
+                    } // end of for
+                } // end of CALLBACK function
+            ); // end of call to $.getJSON     
+        } // end of createCourseTable function
+
+        // Function to create a table for ALL courses.
+        function createCourseTableAll() {
+            let objs;
+            //Starts the communication to the server
+            $.getJSON(
+                "/api/courses",
+                //This function doesn't necessarily run instantaneously
+                function(data) {
+                    objs = data;
+                    $("#courseTableHead").empty();
+                    let markupHeader = "<tr><th>" + "Course ID" + "</th><th>" + "Course Name" + "</th><th>" + "Link" + "</th></tr>";
+                    $("#courseTableHead").append(markupHeader);
+                    $("#courseTableHead").css("font-weight", "bold");
+                    $("#courseTableBody").empty();
+                    for (let i = 0; i < objs.length; i++) {
+                        let url = "details.html?courseid=" + objs[i].CourseId;
+                        let markupBody = "<tr><td>" + objs[i].CourseId + "</td><td>" + objs[i].Title + "</td><td><a href=" + url + ">View Details</a></td >< /tr>";
+                        $("#courseTableBody").append(markupBody);
+                    } // end of for
+                } // end of CALLBACK function
+            ); // end of call to $.getJSON     
+        } // end of createCourseTableAll function
+
+    }) // end of READY EVENT HANDLER
---
+++ public/scripts/details.js
@@ -0,0 +1,91 @@
+"use strict";
+//This script contains code to dynamically create information on different courses from a JSON file.
+//Author:  Pam Belknap
+
+/*
+Key for understanding how to access the fields in the JSON file:
+objs
+objs.length
+objs[i].CourseId
+objs[i].Title
+objs[i].Category
+objs[i].Location
+objs[i].StartDate
+objs[i].EndDate
+objs[i].Meets
+objs[i].Fee
+objs[i].Students (its own array)
+
+Categories:
+categ
+categ.length
+categ[i].Category
+categ[i].Value
+*/
+
+$(function() {
+        let objs;
+
+        //Starts the communication to the server
+        $.getJSON(
+                "/api/courses",
+                //This function doesn't necessarily run instantaneously
+                function(data) {
+                    objs = data;
+
+                    createDetailTable();
+
+                    //This function dynamically creates the course table.
+                    function createDetailTable() {
+                        let urlParams = new URLSearchParams(location.search);
+                        let chosenDetail = urlParams.get("courseid");
+                        let url = "register.html?courseid=" + chosenDetail;
+
+                        $("#courseTableBody").empty();
+                        for (let i = 0; i < objs.length; i++) {
+                            if (chosenDetail == objs[i].CourseId) {
+                                let markupBody0 = "<tr><td class='firstRow'>" + "Course ID" + "</td><td class='firstRow'>" + objs[i].CourseId + "</td></tr>";
+                                $("#courseTableBody").append(markupBody0);
+                                let markupBody1 = "<tr><td>" + "Course Name" + "</td><td id='title' name='title'>" + objs[i].Title + "</td></tr>";
+                                $("#courseTableBody").append(markupBody1);
+                                let markupBody2 = "<tr><td>" + "Category" + "</td><td id='category' name='category'>" + objs[i].Category + "</td></tr>";
+                                $("#courseTableBody").append(markupBody2);
+                                let markupBody3 = "<tr><td>" + "Location" + "</td><td id='location' name='location'>" + objs[i].Location + "</td></tr>";
+                                $("#courseTableBody").append(markupBody3);
+                                let markupBody4 = "<tr><td>" + "Start Date" + "</td><td id='startdate' name='startdate'>" + objs[i].StartDate + "</td></tr>";
+                                $("#courseTableBody").append(markupBody4);
+                                let markupBody5 = "<tr><td>" + "End Date" + "</td><td id='enddate' name='enddate'>" + objs[i].EndDate + "</td></tr>";
+                                $("#courseTableBody").append(markupBody5);
+                                let markupBody6 = "<tr><td>" + "Days and Time" + "</td><td id='meets' name='meets'>" + objs[i].Meets + "</td></tr>";
+                                $("#courseTableBody").append(markupBody6);
+                                let markupBody7 = "<tr><td>" + "Class Fee" + "</td><td id='fee' name='fee'>" + objs[i].Fee + "</td></tr>";
+                                $("#courseTableBody").append(markupBody7);
+                                let markupBody8 = "<tr><td>" + "Students Registered" + "</td><td id='studentCnt' name='studentCnt'></td></tr>";
+                                $("#courseTableBody").append(markupBody8);
+                                if (objs[i].Students.length == 0) {
+                                    $("#studentCnt").html("No Students Registered");
+                                    $("#studentsTable").hide();
+                                } else {
+                                    $("#studentCnt").html(objs[i].Students.length);
+                                }
+                                $("#studentTableHead").empty();
+                                let markupHeader = "<tr><th>" + "Student Name" + "</th><th>" + "Student Email" + "</th></tr>";
+                                $("#studentTableHead").append(markupHeader);
+                                $("#studentTableHead").css("font-weight", "bold");
+                                $("#studentTableBody").empty();
+                                for (let j = 0; j < objs[i].Students.length; j++) {
+                                    let markupBody9 = "<tr><td>" + objs[i].Students[j].StudentName + "</td><td>" + objs[i].Students[j].Email + "</td></tr>";
+                                    $("#studentTableBody").append(markupBody9);
+                                } // end of if for student table load
+                            } // end of if for table load
+                        } // end of for (table)
+                        $("#btnRegister").on("click", registerForCourse);
+                        //when REGISTER button is clicked:
+                        function registerForCourse() {
+                            location.href = url;
+                        } // end of registerForCourse Function
+                    } // end of function
+                } // end of function(data)
+            ) // end of .getJSON
+
+    }) // end of READY EVENT HANDLER
